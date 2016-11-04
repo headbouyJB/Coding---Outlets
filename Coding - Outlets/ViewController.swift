fe78b7ed83224edb050ebe07feb5661577021cdc
Jesse Bryce <bryce.jesse@gmail.com>
2016-11-04 13:23:37 +1300

    First Step

--- Coding - Outlets/ViewController.swift
+++ Coding - Outlets/ViewController.swift
@@ -3,13 +3,26 @@
 //  Coding - Outlets
 //
 //  Created by Jesse Bryce on 4/11/16.
-//  Copyright © 2016 Jesse Bryce. All rights reserved.
+//  Copyright © 2016 Jesse Bryce. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+   
+    var tapCount = 5
+    
+    @IBAction func buttonPush(_ sender: UIButton) {
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
